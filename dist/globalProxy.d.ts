ae5752bfab7b5894515bb0e1a9a2822c930702d5
yangxu3 <yangxu3@100tal.com>
2020-08-15 19:42:41 +0800

    bugfix

--- dist/globalProxy.d.ts
+++ dist/globalProxy.d.ts
@@ -2,5 +2,5 @@ import { HtmlElementType } from "./type";
 export default function <T extends HtmlElementType>(target: T): {
     sandbox: T;
     reset(): void;
-    setGetProperty(cb: (originalTarget: T, p: PropertyKey) => any): void;
+    setGetProperty(cb: (originalTarget: T, p: string | number | symbol) => any): void;
 };
--- src/index.ts
+++ src/index.ts
@@ -18,8 +18,17 @@ export default {
   create(source: string): ISandbox {
     // 创建全局代理对象，监控全局属性的变化
     const windowProxy = createSandbox(window);
-    const documentProxy = createSandbox(window.document);
-    const bodyProxy = createSandbox(window.document.body);
+    /**
+     * element-ui的popper模块，isFixed方法里有如下判断：
+     *  if (element === root.document.body) {
+     *      return false;
+     *  }
+     * 由于这里的element是递归查询的parentNode，最终会递归到真实的body节点上，而这里的root.document.body由于运行在sandbox中，body是proxy的对象，这里的判断肯定不过了，因此报错：Failed to execute 'getComputedStyle' on 'Window': parameter 1 is not of type 'Element'
+     *
+     * 因此这里暂时不代理document和body元素了
+     */
+    // const documentProxy = createSandbox(window.document);
+    // const bodyProxy = createSandbox(window.document.body);
     // 监听定时器的调用
     const timerWatcher = hackTimer(window);
     // 监控事件的绑定/解绑
@@ -30,8 +39,6 @@ export default {
     windowProxy.setGetProperty((t, p) => {
       if (p === 'top' || p === 'window' || p === 'self') {
         return windowProxy.sandbox;
-      } else if (p === 'document') {
-        return documentProxy.sandbox;
       } else if (p === 'setInterval') {
         return timerWatcher.hookSetInterval;
       } else if (p === 'setTimeout') {
@@ -46,38 +53,40 @@ export default {
         return t.Object;
       }
     });
-    documentProxy.setGetProperty((t, p) => {
-      if (p === 'body') {
-        return bodyProxy.sandbox;
-      } else if (p === 'addEventListener') {
-        return eventWatcherOfDocument.hookAddEventListener;
-      } else if (p === 'removeEventListener') {
-        return eventWatcherOfDocument.hookRemoveEventListener;
-      }
-    });
-    bodyProxy.setGetProperty((t, p) => {
-      if (p === 'addEventListener') {
-        return eventWatcherOfBody.hookAddEventListener;
-      } else if (p === 'removeEventListener') {
-        return eventWatcherOfBody.hookRemoveEventListener;
-      }
-    });
+    windowProxy.sandbox.document.addEventListener = eventWatcherOfDocument.hookAddEventListener;
+    windowProxy.sandbox.document.removeEventListener = eventWatcherOfDocument.hookRemoveEventListener;
+    windowProxy.sandbox.document.body.addEventListener = eventWatcherOfBody.hookAddEventListener;
+    windowProxy.sandbox.document.body.removeEventListener = eventWatcherOfBody.hookRemoveEventListener;
+    // documentProxy.setGetProperty((t, p) => {
+    //   if (p === 'addEventListener') {
+    //     return eventWatcherOfDocument.hookAddEventListener;
+    //   } else if (p === 'removeEventListener') {
+    //     return eventWatcherOfDocument.hookRemoveEventListener;
+    //   }
+    // });
+    // bodyProxy.setGetProperty((t, p) => {
+    //   if (p === 'addEventListener') {
+    //     return eventWatcherOfBody.hookAddEventListener;
+    //   } else if (p === 'removeEventListener') {
+    //     return eventWatcherOfBody.hookRemoveEventListener;
+    //   }
+    // });
 
     return {
       mount() {
-        const wrapperFunction = function(window: any, document: any, setInterval: any, setTimeout: any) {
+        const wrapperFunction = function(window: any, setInterval: any, setTimeout: any) {
           return eval(source);
         };
-        return wrapperFunction.call(windowProxy.sandbox, windowProxy.sandbox, documentProxy.sandbox, timerWatcher.hookSetInterval, timerWatcher.hookSetTimeout);
+        return wrapperFunction.call(windowProxy.sandbox, windowProxy.sandbox, timerWatcher.hookSetInterval, timerWatcher.hookSetTimeout);
       },
       mountWithCommonjs(module: any, exports: any, require: any) {
-        const wrapperFunction = function(window: any, document: any, setInterval: any, setTimeout: any, module: any, exports: any, require: any) {
+        const wrapperFunction = function(window: any, setInterval: any, setTimeout: any, module: any, exports: any, require: any) {
           eval(source);
         };
-        wrapperFunction.call(windowProxy.sandbox, windowProxy.sandbox, documentProxy.sandbox, timerWatcher.hookSetInterval, timerWatcher.hookSetTimeout, module, exports, require);
+        wrapperFunction.call(windowProxy.sandbox, windowProxy.sandbox, timerWatcher.hookSetInterval, timerWatcher.hookSetTimeout, module, exports, require);
       },
       unmount() {
-        [ windowProxy, documentProxy, bodyProxy, timerWatcher, eventWatcherOfWindow, eventWatcherOfDocument, eventWatcherOfBody ].forEach(m => {
+        [ windowProxy, timerWatcher, eventWatcherOfWindow, eventWatcherOfDocument, eventWatcherOfBody ].forEach(m => {
           m.reset();
         });
       }
